fa1bc5e1c14a3e508ddde1acfe2a73878050d6c2
halohack <79331796+halocrak@users.noreply.github.com>
2021-08-22 04:01:25 +0300

    Add files via upload

---
+++ tor.py
@@ -0,0 +1,30 @@
+from random import *
+M=["0780"]
+for o in range (100000):
+    out = str(randint(0,9999999))
+    out2 =(choice(M)+out)
+    print(out2 +":"+ out2)
+from random import *
+M=["0770"]
+for o in range (100000):
+    out = str(randint(0,9999999))
+    out2 =(choice(M)+out)
+    print(out2 +":"+ out2)
+from random import *
+M=["0781"]
+for o in range (100000):
+    out = str(randint(0,9999999))
+    out2 =(choice(M)+out)
+    print(out2 +":"+ out2)
+from random import *
+M=["0771"]
+for o in range (100000):
+    out = str(randint(0,9999999))
+    out2 =(choice(M)+out)
+    print(out2 +":"+ out2)
+from random import *
+M=["0772"]
+for o in range (100000):
+    out = str(randint(0,9999999))
+    out2 =(choice(M)+out)
+    print(out2 +":"+ out2)
