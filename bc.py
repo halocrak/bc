d01bedf4ac739ee4f49899408ce74504d2980e00
halohack <79331796+halocrak@users.noreply.github.com>
2021-08-22 03:39:01 +0300

    Add files via upload

---
+++ bc.py
@@ -0,0 +1,14 @@
+import time
+
+print("\x1b[31;1mBosTa Ta Tool AkreTo")
+time.sleep(1)
+print(" \033[92mTawaw Tool Krayawa")
+
+
+
+z = int(input("FIRST NUMBER: "))
+b = int(input("SECOND NUMBER: "))
+
+halo = z+b
+
+print(halo)
